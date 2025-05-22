8f83093718ac43a6d143440597031e836e16318d
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-05-22 16:57:24 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/tools/visualizer/entrypoint.sh
+++ docker/tools/visualizer/entrypoint.sh
@@ -90,7 +90,7 @@ else
     configure_vnc
     if [ -f /tmp/.X11-unix/X0 ]; then
         echo "X11 display found"
-        falkon --no-sandbox --fullscreen --new-window "http://localhost:6080/vnc.html?resize=scale&password=${REMOTE_PASSWORD}&autoconnect=true"
+        DISPLAY=:0 falkon --no-sandbox --fullscreen --new-window "http://localhost:6080/vnc.html?resize=scale&password=${REMOTE_PASSWORD}&autoconnect=true"
     else
         echo "No X11 display found"
     fi
